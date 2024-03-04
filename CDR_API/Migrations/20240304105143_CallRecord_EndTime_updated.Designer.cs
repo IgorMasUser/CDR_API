76331dae0b8261011ca47bd55542cd20065a9af9
Igor <igormasmas1@gmail.com>
2024-03-04 11:57:00 +0100

    DB updated

--- CDR_API/Migrations/20240304105143_CallRecord_EndTime_updated.Designer.cs
+++ CDR_API/Migrations/20240304105143_CallRecord_EndTime_updated.Designer.cs
@@ -4,6 +4,7 @@ using CDR_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CDR_API.Migrations
 {
     [DbContext(typeof(CdrContext))]
-    partial class CdrContextModelSnapshot : ModelSnapshot
+    [Migration("20240304105143_CallRecord_EndTime_updated")]
+    partial class CallRecord_EndTime_updated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
